7c034c0f3772c5b68acbea99b9d8a33ee7a6b94f
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-07-01 13:30:01 -0400

    Clean up

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -11,14 +11,11 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option(
-    "sqlalchemy.url",
-    os.environ["DATABASE_URL"],
-)
+config.set_main_option("sqlalchemy.url",os.environ["DATABASE_URL"])
 
 from modules.models import Base
 
-target_metadata = Base.metadata
+target_metadata = Base.metadata  
 
 
 def runOfflineMigrations() -> None:
@@ -36,22 +33,15 @@ def runOfflineMigrations() -> None:
 
 
 def runOnlineMigrations() -> None:
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = engine_from_config(config.get_section(config.config_ini_section, {}), prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.begin() as connection:
-        connection.execute(
-            sa.text('CREATE SCHEMA IF NOT EXISTS "LinkDB"')
-        )
+        connection.execute(sa.text('CREATE SCHEMA IF NOT EXISTS "LinkDB"'))
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-        )
+        context.configure(connection=connection,target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
